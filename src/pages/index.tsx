970803b20b6970ea31a2a35a04c0547ba7fea5ec
rodrigo kalil <rkbuarque@gmail.com>
2021-08-17 13:35:46 -0300

    scoped css e sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1>nextjs</h1>
+    <h1 className={styles.title}>
+      nextjs
+    </h1>
   )
 }
