69d54969611f402865d0a49da3d694d4974dd712
rodrigo kalil <rkbuarque@gmail.com>
2021-08-21 22:00:42 -0300

    Salvando usuário no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,7 +15,31 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY,
+  },
 
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+
+      } catch {
+        return false
+      }
+
+    },
+  },
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
 })
