78b58304c8369bd0c3c4668942dd5e491ec44e46
rodrigo kalil <rkbuarque@gmail.com>
2021-08-24 22:06:38 -0300

    webhooks do stripe

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, resp: NextApiResponse) => {
+    console.log('evento recebido')
+
+    resp.status(200).json({ok: true})
+}
