75aa2374e4fc77b46afe8ebda52ab33f50fce649
rodrigo kalil <rkbuarque@gmail.com>
2021-08-27 03:06:56 -0300

    criando o componente ActiveLink e corrigindo o css active do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link'
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
+
 
 export function Header() {
     return(
@@ -8,13 +9,13 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                 <nav>
-                    <Link href="/" >
-                        <a className={styles.active}>Home</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active} href="/" >
+                        <a>Home</a>
+                    </ActiveLink>
 
-                    <Link href="/posts" prefetch>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
                         <a>Posts</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
