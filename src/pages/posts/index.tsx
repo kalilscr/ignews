68ab3078aca6adebad9b4cde8bf776760c53dedf
rodrigo kalil <rkbuarque@gmail.com>
2021-08-27 01:47:22 -0300

    Consumindo API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,9 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import React from 'react'
+import Prismic from '@prismicio/client';
 import styles from './styles.module.scss'
+import { getPrismicClient } from '../../services/prismic';
 
 export default function Posts() {
     return(
@@ -31,3 +34,19 @@ export default function Posts() {
         </>
     )
 } 
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'post')
+    ], {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+    })    
+    
+    console.log(response)
+    return {
+        props: {}
+    }
+}
